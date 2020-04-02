0f81cf36b2d47377c0396e19c57973b75968f5d4
Roberto Livi <robertomlivi@outlook.com>
2020-04-02 00:38:38 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -22,7 +22,7 @@ class App < Sinatra::Base
   get '/mulitply/:num1/:num2' do
     @num_1 = params[:num1]
     @num_2 = params[:num2]
-    @num_1.to_i * @num_2.to_i
+    "#{@num_1.to_i} * #{@num_2.to_i}"
   end
 
 end
